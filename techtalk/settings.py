9f61c4a30155865f4e550859265d497f690a0f9d
Ferdinand <ferdinandchidera49@gmail.com>
2022-11-03 16:17:11 +0100

    encrypted the secret key and static files

--- techtalk/settings.py
+++ techtalk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1*zufzh92^ibzj=1_(tmffeh0ih!n-m+r%aem+t5ueu#)+j^o8' #os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
