22d61176f85b9451f5791281d3b5f3c9f1f47d62
Ferdinand <ferdinandchidera49@gmail.com>
2022-11-03 16:23:42 +0100

    added database

--- techtalk/settings.py
+++ techtalk/settings.py
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'techtalk.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['PGDATABASE'],
+        'USER': os.environ['PGUSER'],
+        'PASSWORD': os.environ['PGPASSWORD'],
+        'HOST': os.environ['PGHOST'],
+        'PORT': os.environ['PGPORT'], 
+      }
 }
 
 
